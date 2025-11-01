6ba6dc960775cd982ecf0396a0c9cffb01fb0701
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-01 20:04:04 +0200

    chore: cleanup sample test and fix Jest TMPDIR permissions

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  cacheDirectory: '<rootDir>/.jest-cache',
+};
+
+export default config;
